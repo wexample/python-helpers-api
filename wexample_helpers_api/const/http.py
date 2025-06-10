b2df69ff9c2ab5bdfdab063ad8acb193b9325ac7
weeger <romain.weeger@gmail.com>
2025-06-10 15:08:48 +0200

    Get model enum

---
+++ wexample_helpers_api/const/http.py
@@ -0,0 +1,11 @@
+from wexample_helpers_api.enums.http import HttpMethod
+
+HTTP_METHOD_MAP = {
+    "GET": HttpMethod.GET,
+    "POST": HttpMethod.POST,
+    "PUT": HttpMethod.PUT,
+    "DELETE": HttpMethod.DELETE,
+    "PATCH": HttpMethod.PATCH,
+    "OPTIONS": HttpMethod.OPTIONS,
+    "HEAD": HttpMethod.HEAD
+}
